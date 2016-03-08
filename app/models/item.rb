6c28f56538a2659e2a6117e9ac4d9f2ac118d5bc
silentfl <silentfl@mail.ru>
2016-03-08 23:18:24 +0300

    Простая валидация входных данных

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ActiveRecord::Base 
+  validates :long_url, presence: true
+  validates :long_url, format: { with: URI.regexp }, if: Proc.new { |a| a.long_url.present? } #TODO фильтровать /^data|^javascript/i
 end
